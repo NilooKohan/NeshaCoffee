ef4e99f6d7e50b3556e06936ab34f53dd1da3a32
Niloo Kohan <n.falahkohan@gmail.com>
2021-11-16 20:21:52 +0100

    neues Menu

---
+++ custom.js
@@ -0,0 +1,20 @@
+/*$.ajax({
+    url: 'https://opensheet.vercel.app/1ogm0p7gcYNyzndemZ1dtfI8TZMR9x_JFEVjsoMV-rS8/NeshaCoffeeMenu',
+    dataType: 'json',
+    success: function (data) {
+        console.log(data);
+        $.each(data, function (key, NeshaCoffeeMenu) {
+            console.log(NeshaCoffeeMenu.Title)
+            $('#Coffees').append('<div class="col-md-3 text-center' >
+                <img src="${Menu.PicURL}" alt="" class="img-fluid rounded-circle">
+                    <h3>${NeshaCoffeeMenu.Title}</h3>
+                    <p>€ ${NeshaCoffeeMenu.Price}</p>
+                    <p>${NeshaCoffeeMenu.Description}</p>
+                </img> 
+        </div > ')'
+
+  })
+
+    }
+});*/
+
